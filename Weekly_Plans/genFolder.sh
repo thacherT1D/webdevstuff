5995253a0e8144cbb768425449d1a25bdbd07794
Roger Schmidt <rogerschmidt@gmail.com>
2016-04-15 16:22:06 -0600

    Delete genFolder.sh

--- Weekly_Plans/genFolder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir Week_`echo $1`
-touch Week_`echo $1`/Week_`echo $1`_Plan.desc.md
-echo "# Week `echo $1`" > Week_`echo $1`/Week_`echo $1`_Plan.desc.md
-touch Week_`echo $1`/Week_`echo $1`_Plan.notes.md
-echo "# Week `echo $1` - Instructor Notes" > Week_`echo $1`/Week_`echo $1`_Plan.notes.md
