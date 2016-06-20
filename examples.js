b13fff7ff06f8fb6ba3ac5cd6227b2ffdeadc40c
Jeff Dean <jeff@galvanize.com>
2016-06-20 17:11:23 -0600

    ducks

---
+++ examples.js
@@ -0,0 +1,52 @@
+let input = [
+  {id: 1, name: "A"},
+  {id: 2, name: "B"}
+]
+
+let obj = {}
+for (var i = 0; i < input.length; i++) {
+  obj[input[i].id] = input[i]
+  // obj[input[0].id] = input[i]
+  // obj[{id: 1, name: "A"}.id] = input[i]
+  // obj[1] = input[i]
+}
+
+// obj
+
+
+obj1, obj2
+
+for (var key in obj1) {
+  return false if obj2[key] !== obj1[key]
+}
+for (var key in obj2) {
+  return false if obj2[key] !== obj1[key]
+}
+return true
+
+
+
+let i = 0
+function b(num) {
+  num++
+}
+b(i)
+console.log(i);
+
+
+
+
+
+const obj1 = {}
+const obj2 = {}
+
+obj1.next = obj2
+obj1.val = "cat"
+obj2.prev = obj1
+obj2.val = "kitty"
+
+
+
+
+
+// -----
