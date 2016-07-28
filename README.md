9037402db59088e76e68771b3d68a5c733ef782b
Jeff Dean <jeff@galvanize.com>
2016-07-28 11:54:58 -0600

    created simple build script to add tables to readmes

--- README.md
+++ README.md
@@ -1,11 +1,52 @@
 # Galvanize Web Development Immersive
 
-- [Quarter 1](Schedule/Q1/readme.md)
-  - [Quarter 1 Project](Schedule/Q1/Q1 Project.md)
-  - [Learning to Learn](Learning to Learn.md)
-  - [Setup Development Environment](Development Environment/README.md)
-- [Quarter 2](Schedule/Q2/readme.md)
-  - [Quarter 2 Project](Schedule/Q2/Q2 Project.md)
-- [Quarter 3](Schedule/Q3/readme.md)
-- [Quarter 4](Schedule/Q4/readme.md)
-  - [Quarter 4 Project](Schedule/Q4/Capstone Project.md)
+<!-- BEGIN SCHEDULE -->
+<table>
+  <thead>
+    <tr>
+      <th>Day</th>
+      <th>Warmup</th>
+      <th>Activities</th>
+      <th>Exercises</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th rowspan="3">Monday</th>
+      <td rowspan="3">
+      </td>
+      <td><a href="https://docs.google.com/presentation/d/154ou9yQJNcVcVehD6vqaKjGbCKhFb2xK85toqniWaa8/edit#slide=id.g108a6e17ae_0_112">Welcome to WDI</a>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td><a href="Learning to Learn.md">Learning to Learn</a>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td><a href="Development Environment/README.md">Setup Development Environment</a>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <th rowspan="2">Tuesday</th>
+      <td rowspan="2"><a>Typing</a>
+      </td>
+      <td><a href="Development Environment/README.md">Setup Development Environment</a>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td><a href="Development Environment/README.md">Intro to the Command Line</a>
+      </td>
+      <td><a href="https://github.com/ryansobol/clmystery">Command Line Murder Mystery</a>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<!-- END SCHEDULE -->
---
+++ build/schedule.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const pug = require('pug');
+const path = require('path')
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+function q1() {
+  return [
+    {
+      activities: [
+        {article: {text: "Welcome to WDI", url: "https://docs.google.com/presentation/d/154ou9yQJNcVcVehD6vqaKjGbCKhFb2xK85toqniWaa8/edit#slide=id.g108a6e17ae_0_112"}},
+        {article: {text: "Learning to Learn", path: "Learning to Learn.md"}},
+        {article: {text: "Setup Development Environment", path: "Development Environment/README.md"}},
+      ]
+    },
+    {
+      warmup: { text: "Typing", path: "Typing.md" },
+      activities: [
+        {
+          article: {text: "Setup Development Environment", path: "Development Environment/README.md"},
+        },
+        {
+          article: {text: "Intro to the Command Line", path: "Development Environment/README.md"},
+          exercise: {text: "Command Line Murder Mystery", url: "https://github.com/ryansobol/clmystery"},
+        },
+      ],
+    },
+  ]
+}
+
+function normalize(path, base) {
+  return '../'.repeat(base) + path
+}
+
+function dataFor(base) {
+  let result = q1()
+  result.forEach(function (day, i) {
+    day.name = days[i % 5]
+
+    if (day.warmup && day.warmup.path) {
+      normalize(day.warmup.path, base)
+    }
+
+    day.activities.forEach(function (activity) {
+      if (activity.article && activity.article.path) {
+        activity.article.url = normalize(activity.article.path, base)
+      }
+      if (activity.exercise && activity.exercise.path) {
+        activity.exercise.url = normalize(activity.exercise.path, base)
+      }
+    })
+
+    day.firstActivity = day.activities[0]
+    day.otherActivities = day.activities.slice(1)
+  })
+
+  return result
+}
+
+function renderTo(path, template, base) {
+  let html = template({data: dataFor(base)});
+
+  const file = fs.readFileSync(path, 'utf8')
+  const lines = file.split("\n")
+  const startLine = lines.indexOf('<!-- BEGIN SCHEDULE -->')
+  const endLine = lines.indexOf('<!-- END SCHEDULE -->')
+
+  lines.splice(startLine + 1, endLine - startLine - 1)
+  lines.splice(startLine + 1, 0, html.trim())
+
+  console.log(lines.join("\n"));
+  fs.writeFileSync(path, lines.join('\n'))
+}
+
+const templatePath = path.format({root: __dirname, base: '/table.pug'})
+let template = pug.compileFile(templatePath, {pretty: true});
+
+renderTo('./README.md', template, 0)
+renderTo('./Schedule/Q1/readme.md', template, 2)
